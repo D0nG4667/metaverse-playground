0fb3685e07a7cd85d6355951dc4fb6b75ec11136
Gabriel Okundaye <gabriel007okuns@gmail.com>
2021-12-27 03:01:22 +0100

    complete metaverse login screen UI

---
+++ components/login.js
@@ -0,0 +1,29 @@
+import Image from "next/image"
+
+function login() {
+    return (
+        <div className="bg-black relative">
+            
+            <div className="flex flex-col absolute z-50 h-4/6 w-full items-center justify-center space-y-4">
+                
+                <Image 
+                    className="object-cover rounded-full"
+                    src="https://links.papareact.com/3pi"
+                    width={200}
+                    height={200}
+                />
+                
+                <button className="bg-yellow-500 rounded-lg p-5 font-bold animate-pulse">Login to the METAVERSE</button>
+            </div>
+            <div className="w-full h-screen">
+                <Image 
+                    src="https://links.papareact.com/55n"
+                    layout="fill"
+                    objectFit="cover"
+                />
+            </div>
+        </div>
+    )
+}
+
+export default login
